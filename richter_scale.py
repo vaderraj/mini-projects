32724ef504ce7fab9dfc18b51d9aebdec590494d
Aman Raj <amanrajonworld@gmail.com>
2020-08-19 13:02:12 +0530

    final commit

--- richter_scale.py
+++ richter_scale.py
@@ -4,5 +4,14 @@ def richter_to_joule(r):
 def richter_to_tnt(r):
     return  richter_to_joule(r)/4.184e9
 
+arr = []
+arr.append(['Richter', 'Joules', 'TNT'])
+for i in (1, 5, 9.1, 9.2, 9.5):
+    arr.append([str(i), str(richter_to_joule(i)), str(richter_to_tnt(i))])
+col_width = max(len(word) for row in arr for word in row) + 2
+for row in arr:
+    print(''.join(word.ljust(col_width) for word in row))
 r = float(input('Please enter a Richter scale value:'))
-print('{:<}{:>4}'.format{'Richter scale value:', r})
+print('Richter scale value: ' + str(r))
+print('Equivalence in joules: ' + str(richter_to_joule(r)))
+print('Equivalence in tons of TNT: ' + str(richter_to_tnt(r)))
